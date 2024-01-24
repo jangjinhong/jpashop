d983672c5d5cdeb3db7e728af095a80424ba60ae
jangjinhong <honge1122@naver.com>
2024-01-24 15:48:56 +0900

    섹션 4. 회원 리포지토리 개발 및 기존 코드 리팩토링

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -5,6 +5,8 @@ import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
 public class OrderItem {
@@ -13,12 +15,12 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
     // OrderItem과 Order -> 1개주문=n개주문상품으로이루어짐. 다대일 양방향 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 

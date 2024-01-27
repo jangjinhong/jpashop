69a51a6683ff096bf101139852caaa9db0596e65
jangjinhong <honge1122@naver.com>
2024-01-27 14:55:30 +0900

    섹션 6. 주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -19,7 +22,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
-    // OrderItem과 Order -> 1개주문=n개주문상품으로이루어짐. 다대일 양방향 
+    // OrderItem과 Order -> 1개주문=n개주문상품으로 이루어짐. 다대일 양방향
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
@@ -27,4 +30,25 @@ public class OrderItem {
     private int orderPrice; //주문 가격
     private int count;  //주문 수량
 
+
+    /* -- 생성 메서드 -- */
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+
+        return orderItem;
+    }
+
+    /* --- 비즈니스 로직 --- */
+    public void cancel() {
+        getItem().addStock(count);  // 재고 수량 다시 돌려놓기
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
